1a9dd412da566fc10a55ae334be649897577232b
Nick <nick@gmail.com>
2017-11-30 14:58:09 -0600

    Add Fish.java and Swimmable.java

---
+++ src/main/java/edu/utexas/ece/Fish.java
@@ -0,0 +1,26 @@
+package edu.utexas.ece;
+
+public class Fish extends Animal implements Swimmable {
+    public Fish() {
+        super();
+    }
+
+    public Fish(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String say() {
+        return "Bulo";
+    }
+
+    @Override
+    public boolean canSwim() {
+        return true;
+    }
+
+    @Override
+    public int speed() {
+        return 2;
+    }
+}
---
+++ src/main/java/edu/utexas/ece/Swimmable.java
@@ -0,0 +1,6 @@
+package edu.utexas.ece;
+
+public interface Swimmable {
+    boolean canSwim();
+    int speed();
+}
